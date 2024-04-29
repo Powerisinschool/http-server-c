9335c04ee3f7cf0b9ae017500df0f7fee1b0435c
Tolulope Olagunju <toolulopeolagunju@gmail.com>
2024-04-29 13:12:01 +0100

    pass stage

--- app/server.c
+++ app/server.c
@@ -6,17 +6,59 @@
 #include <string.h>
 #include <errno.h>
 #include <unistd.h>
+#include <pthread.h>
 
 #define BUF_SIZE 500
 
-// static const char *HTTP_STATUS = "HTTP/1.1 200 OK";
-// static const char *HEADERS = "";
 static const char *HTTP_200_RESPONSE = "HTTP/1.1 200 OK\r\n\r\n";
 static const char *HTTP_404_RESPONSE = "HTTP/1.1 404 Not Found\r\n\r\n";
 
-// char *respond(int status) {
-// 	return "HTTP/1.1 " + "status" + " OK\r\n\r\n";
-// }
+void *handle_connection(void *arg) {
+	int client_fd = *((int *)arg);
+    char buf[BUF_SIZE];
+	char *buf2 = malloc(sizeof(char) * BUF_SIZE);
+    char *path;
+	char *response = malloc(sizeof(char)*500);
+	char *userAgent = malloc(sizeof(char)*500);
+
+	if (recv(client_fd, buf, BUF_SIZE, 0) == -1) {
+		printf("Receiving failed: %s \n", strerror(errno));
+		close(client_fd);
+		return NULL;
+	}
+
+	strcpy(buf2, buf);
+	
+	for (char *header = strtok(buf2, "\r\n"); header != NULL; header = strtok(NULL, "\r\n")) {
+		// printf("Header Value: %s\n\n", header);
+		if (strncmp(header, "User-Agent", 10) == 0) {
+			userAgent = header + 12;
+			// printf("User Agent: %s\n", userAgent);
+		}
+	}
+
+	if (strtok(buf, " ") == NULL) {
+        printf("Invalid request\n");
+        close(client_fd);
+        return NULL;
+    }
+	path = strtok(NULL, " ");
+
+	if (strncmp(path, "/echo/", 6) == 0) {
+		const char *content = path + 6;
+		sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", strlen(content), content);
+	} else if (strcmp(path, "/user-agent") == 0) {
+		sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", strlen(userAgent), userAgent);
+	} else if (strcmp(path, "/") == 0) {
+		response = (char *)HTTP_200_RESPONSE;
+	} else {
+		response = (char *)HTTP_404_RESPONSE;
+	}
+	write(client_fd, (const char *)response, strlen(response));
+
+	close(client_fd);
+	return NULL;
+}
 
 int main() {
 	// char *HTTP_RESPONSE;
@@ -29,14 +71,16 @@ int main() {
 
 	// Uncomment this block to pass the first stage
 	//
-	int server_fd, client_fd, client_addr_len;
+	int server_fd, client_fd;
 	struct sockaddr_in client_addr;
-	char buf[BUF_SIZE];
-	char *buf2 = malloc(sizeof(char) * BUF_SIZE);
-	char *header = malloc(sizeof(char) * 100);
-	char *response = malloc(sizeof(char)*500);
-	char *userAgent = malloc(sizeof(char)*500);
-	char *path = malloc(sizeof(char) * 100);
+    socklen_t client_addr_len = sizeof(client_addr);
+    pthread_t tid;
+	// char buf[BUF_SIZE];
+	// char *buf2 = malloc(sizeof(char) * BUF_SIZE);
+	// char *header = malloc(sizeof(char) * 100);
+	// char *response = malloc(sizeof(char)*500);
+	// char *userAgent = malloc(sizeof(char)*500);
+	// char *path = malloc(sizeof(char) * 100);
 
 	server_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if (server_fd == -1) {
@@ -68,52 +112,56 @@ int main() {
 		return 1;
 	}
 	
-	printf("Waiting for a client to connect...\n");
+	printf("Waiting for clients to connect...\n");
 
-	client_addr_len = sizeof(client_addr);
-	
-	client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-	if (client_fd == -1) {
-		printf("Failed to connect to the active client: %s...\n", strerror(errno));
-		return 1;
+	while(1) {
+		client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
+		if (client_fd == -1) {
+			printf("Failed to connect to the active client: %s...\n", strerror(errno));
+			continue;
+		}
+		if (pthread_create(&tid, NULL, handle_connection, &client_fd) != 0) {
+            printf("Failed to create thread for handling connection\n");
+            close(client_fd);
+        }
+		printf("Client connected\n");
 	}
-	printf("Client connected\n");
 
 	/* Handle receiving data */
-	if (recv(client_fd, buf, BUF_SIZE, 0) == -1) {
-		printf("Receiving failed: %s \n", strerror(errno));
-		return 1;
-	}
+	// if (recv(client_fd, buf, BUF_SIZE, 0) == -1) {
+	// 	printf("Receiving failed: %s \n", strerror(errno));
+	// 	return 1;
+	// }
 
 	// printf("\n=====\n%s\n=====\n", buf);
 
-	strcpy(buf2, buf);
+	// strcpy(buf2, buf);
 	
-	for (header = strtok(buf2, "\r\n"); header != NULL; header = strtok(NULL, "\r\n")) {
-		// printf("Header Value: %s\n\n", header);
-		if (strncmp(header, "User-Agent", 10) == 0) {
-			userAgent = header + 12;
-			// printf("User Agent: %s\n", userAgent);
-		}
-	}
+	// for (header = strtok(buf2, "\r\n"); header != NULL; header = strtok(NULL, "\r\n")) {
+	// 	// printf("Header Value: %s\n\n", header);
+	// 	if (strncmp(header, "User-Agent", 10) == 0) {
+	// 		userAgent = header + 12;
+	// 		// printf("User Agent: %s\n", userAgent);
+	// 	}
+	// }
 
-	strtok(buf, " ");
-	path = strtok(NULL, " ");
+	// strtok(buf, " ");
+	// path = strtok(NULL, " ");
 	// printf("Path: %s\n", path);
 
-	if (strncmp(path, "/echo/", 6) == 0) {
-		const char *content = path + 6;
-		// response = (char *)HTTP_200_RESPONSE;
-		sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", strlen(content), content);
-	} else if (strcmp(path, "/user-agent") == 0) {
-		sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", strlen(userAgent), userAgent);
-	} else if (strcmp(path, "/") == 0) {
-		// printf("200 OK\n");
-		response = (char *)HTTP_200_RESPONSE;
-	} else {
-		response = (char *)HTTP_404_RESPONSE;
-	}
-	write(client_fd, (const char *)response, strlen(response));
+	// if (strncmp(path, "/echo/", 6) == 0) {
+	// 	const char *content = path + 6;
+	// 	// response = (char *)HTTP_200_RESPONSE;
+	// 	sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", strlen(content), content);
+	// } else if (strcmp(path, "/user-agent") == 0) {
+	// 	sprintf(response, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %zu\r\n\r\n%s", strlen(userAgent), userAgent);
+	// } else if (strcmp(path, "/") == 0) {
+	// 	// printf("200 OK\n");
+	// 	response = (char *)HTTP_200_RESPONSE;
+	// } else {
+	// 	response = (char *)HTTP_404_RESPONSE;
+	// }
+	// write(client_fd, (const char *)response, strlen(response));
 	
 	
 	close(server_fd);
